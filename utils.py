4147442d5d0b5d7c0822b7e6c1a9478e632103a3
Siddhant Jain <siddhantjain209@gmail.com>
2024-09-09 10:10:00 +0530

    refactor: Minor change in log name

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ os.makedirs(LOGS_DIR, exist_ok=True)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[
-                        logging.FileHandler(os.path.join(LOGS_DIR, f"{datetime.now().strftime('%d-%m-%Y')}-app.log")),
+                        logging.FileHandler(os.path.join(LOGS_DIR, f"{datetime.now().strftime('%d-%m-%Y')}_app.log")),
                         logging.StreamHandler()
                     ])
 logger = logging.getLogger(__name__)
